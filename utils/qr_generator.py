00d6c304cf2fae508171215450c2a31a05c9e6fa
Ivan Kruglov <kruglovivan005@gmail.com>
2024-01-12 18:16:44 +0300

    qr fixed

--- utils/qr_generator.py
+++ utils/qr_generator.py
@@ -13,12 +13,12 @@ def generate_qr_code(uid: str) -> bytes:
     qr = QRCode(
         error_correction=ERROR_CORRECT_H
     )
-    qr.add_data("tg://resolve?domain=GlassMonitor1502Bot&start=" + uid)
+    qr.add_data("https://t.me/GlassMonitor1502Bot?start=" + uid)
     qr.make(fit=True)
     img = qr.make_image(
         image_factory=StyledPilImage,
         module_drawer=RoundedModuleDrawer(),
-        color_mask=RadialGradiantColorMask(),
+        color_mask=RadialGradiantColorMask(back_color=(255, 255, 255, 0), center_color=(0, 0, 0, 255), edge_color=(0, 0, 255, 255)),
         embeded_image_path=join(getcwd(), "logo.png"),
     )
     buf = io.BytesIO()
